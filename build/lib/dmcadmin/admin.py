d7fcec9c2b8ca5064e87059832e66b44ecb49433
hairetdin <amir.hairetdinov@gmail.com>
2020-04-30 20:24:08 +0500

    remove build

--- build/lib/dmcadmin/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-
-@admin.register(models.ManageTask)
-class ManageTaskAmin(admin.ModelAdmin):
-    pass
--- build/lib/dmcadmin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MCAdminConfig(AppConfig):
-    name = 'dmcadmin'
